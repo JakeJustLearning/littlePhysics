33ffdc49835002735b61c27f978d8081f2b35e3a
JakeJustLearning <jacobkarmelross@gmail.com>
2021-05-28 23:01:38 +0200

    urg

--- app.js
+++ app.js
@@ -1,60 +1,90 @@
+window.maxFPS = 1
+window.lastFrameTimeMS = Date.now()
+    // HARDWARE CLASSES
+
 class Game {
     constructor() {
         this.gameObjects = []
         this.controller = new Controller
+        this.physics = new Engine
+        this.gameOn = true
+
         this.initGame()
     }
 
     initGame() {
-        createGround()
+        this.createWorld()
+        this.createPlayer()
+        this.loopDraw()
+
+        this.mainLoop()
+    }
+
+    mainLoop() {
+        while (this.gameOn == true) {
+            console.log('looping')
+            const timestamp = Date.now()
+
+            if (timestamp < window.lastFrameTimeMs + (1000 / window.maxFPS)) {
+                coninue;
+            }
+            this.loopUpdate()
+            this.loopDraw()
+            window.lastFrameTimeMS = timestamp
+        }
+    }
+
+    // LOGICING
+
+    loopUpdate() {
+        this.applyPlayerInput()
+        this.physics.run(this.gameObjects.filter(gameObj => gameObj.controlling == true))
+    }
+
+
+    applyPlayerInput() {
+        const inputs = this.controller.getInputs()
+        this.gameObjects.filter(gameObj => gameObj.controlling == true)
+            .forEach(gameObj => {
+                gameObj.velX -= inputs[0] ? 1 : 0
+                gameObj.velY += inputs[0] ? 1 : 0
+                gameObj.VelX += inputs[0] ? 1 : 0
+                gameObj.VelY -= inputs[0] ? 1 : 0
+            })
+
 
     }
 
+    // RENDERING
     loopDraw() {
-        canvas.clearRect(0, 0, canvas.width, canvas.height)
-        scrn.lineWidth = 1
-        scrn.strokeStyle = 'lime'
-        scrn.fillStyle = 'lime'
+        console.log('drawing')
+        scrn.clearRect(0, 0, canvas.width, canvas.height)
+
         this.gameObjects.forEach(gameObj => {
             this.drawObject(gameObj)
         })
-        scrn.stroke()
-        scrn.fill()
-        scrn.closePath()
+
     }
 
     drawObject(gamObj) {
+        scrn.lineWidth = 4
+        scrn.strokeStyle = 'lime'
+        scrn.fillStyle = 'lime'
         scrn.beginPath()
         scrn.beginPath()
         scrn.rect(gamObj.x, gamObj.y, gamObj.width, gamObj.height)
+        scrn.stroke()
+        scrn.fill()
+    }
+    createWorld() {
+        const ground = new Rectangle(0, 145, 0, 0, false, canvas.width, 10)
+        this.gameObjects.push(ground)
+    }
+    createPlayer() {
+        const player = new Player(100, 110, 0, 0, true, 25, 30)
+        this.gameObjects.push(player)
     }
 }
-
-class GameObject {
-    constructor(x, y, velX, velY, gravity) {
-        this.x = x
-        this.y = y
-        this.velX = velX || 0
-        this.velY = velY || 0
-        this.shap = ''
-
-        this.isColliding = false
-        this.hasGravity = gravity || true
-
-    }
-}
-
-class Square extends GameObject {
-    constructor(x, y, velX, velY, gravity, width, height) {
-        super(x, y, velX, velY, gravity)
-        this.width = width
-        this.height = height
-        this.shape = 'rectangle'
-    }
-}
-
-
-
 // CONTROLS
 class Controller {
     constructor() {
@@ -67,6 +97,7 @@ class Controller {
     }
 
     keyDown(event) {
+        // if (document.getElementById('play-space').getAttribute('focus-within')) {
         console.log(event.code)
         switch (event.keyCode) {
             case 37:
@@ -85,8 +116,10 @@ class Controller {
                 break
         }
         event.preventDefault()
+            // }
     }
     keyUp(event) {
+        // if (document.getElementById('play-space').getAttribute('focus-within')) {
         console.log(event.code)
         switch (event.keyCode) {
             case 37:
@@ -105,14 +138,99 @@ class Controller {
                 break
         }
         event.preventDefault()
+            // }
+    }
+
+    getInputs() {
+        return [this.left, this.right, this.up, this.down]
     }
 
 }
 
-function initGame() {
-    const canvas = document.querySelector('#game-screen')
-    const scrn = canvas.getContext('2d')
-    const controller = new Controller()
+// SOFTWARE CLASSES
+class GameObject {
+    constructor(x, y, velX, velY, gravity) {
+        this.x = x
+        this.y = y
+        this.velX = velX || 0
+        this.velY = velY || 0
+        this.shap = ''
+
+        this.isColliding = false
+        this.hasGravity = gravity || true
+
+    }
+}
+
+class Rectangle extends GameObject {
+    constructor(x, y, velX, velY, gravity, width, height) {
+        super(x, y, velX, velY, gravity)
+        this.width = width
+        this.height = height
+        this.shape = 'rectangle'
+    }
+}
+
+class Player extends Rectangle {
+    constructor(x, y, velX, velY, gravity, width, height) {
+        super(x, y, velX, velY, gravity, width, height)
+        this.controlling = true
+    }
+
+}
+
+class Engine {
+    constructor() {
+        this.maxVelX = 5
+        this.maxVelY = 12
+        this.gravity = 1.5
+    }
+    run(gameObjects) {
+        gameObjects.forEach(gameObj => {
+            this.yMovement(gameObj)
+            this.xMovement(gameObj)
+            this.friction(gameObj);
+            (gameObj.gravity) ? this.gravity(gameObj): console.log('nograv');
+            if (gameObj.y + gameObj.height > 145) {
+                gameObj.y = 145 - gameObj.height
+                gameObj.VelY = 0
+            }
+        })
+
+    }
+
+    yMovement(gameObject) {
+        gameObject.y += gameObject.velY
+    }
+
+    xMovement(gameObject) {
+        gameObject.x += gameObject.velX
+
+    }
+    friction(gameObject) {
+        gameObject.velX *= 0.8
+        gameObject.velY *= 0.9
+    }
+
+    gravity(gameObject) {
+        gameObject.velY -= 1.5
+    }
+
+}
+
+
+
+
+
+
+// Global Functions
+
+
+function gameOn() {
+    console.log('new game')
+    window.canvas = document.querySelector('#game-screen')
+    window.scrn = canvas.getContext('2d')
+    window.game = new Game
 }
 
 
@@ -160,6 +278,7 @@ $(document).ready(function() {
         console.log('power is clicked')
         if (document.querySelector('#power-light').getAttribute('data-lightOn') == 'true') {
             console.log('power-is-on')
+            game.gameOn = false
             document.querySelector('#power-switch').style.transform = 'translateY(0px)'
             $('#power-light').attr('data-lightOn', false)
             scrn.clearRect(0, 0, canvas.width, canvas.height)
@@ -167,7 +286,7 @@ $(document).ready(function() {
             console.log('power-is-off')
             document.querySelector('#power-switch').style.transform = 'translateY(-20px)'
             $('#power-light').attr('data-lightOn', true);
-            initGame()
+            gameOn()
         }
     })
 
